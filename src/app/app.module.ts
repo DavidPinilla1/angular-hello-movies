3dff8c2543281970c106d5d3150db398abb2cc27
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-07-09 13:12:32 +0200

    added es-Ar as a default locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import localeES from '@angular/common/locales/es-AR';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MovieListComponent } from './containers/movie-list/movie-list.component';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeES);
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,9 @@ import { MovieListComponent } from './containers/movie-list/movie-list.component
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es-AR" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
