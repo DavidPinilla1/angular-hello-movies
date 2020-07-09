c44325b19f0708b2f9d81d0cc9ac3b88ae52f80e
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-07-09 13:57:52 +0200

    add http params to the getMovies httpclient request, also added language type

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 // import { map, filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { MovieResponse } from '../types/movie-response.type';
+type language = 'es-ES' | 'en-EN';
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  API_KEY = 'cea68b520beecac6718820e4ac576c3a';
   constructor(private httpClient: HttpClient) { }
 
-  getMovies(): Observable<MovieResponse> {
-    return this.httpClient.get<MovieResponse>('https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&append_to_response=credits&language=es-ES');
+  getMovies(language: language = 'es-ES', page: string = '1'): Observable<MovieResponse> {
+    // const params = new HttpParams()
+    //   .set('api_key', 'cea68b520beecac6718820e4ac576c3a')
+    //   .set('language', language)
+    //   .set('page', page);
+    const params = {
+      api_key: this.API_KEY,
+      language,
+      page,
+    };
+    return this.httpClient.get<MovieResponse>('https://api.themoviedb.org/3/movie/popular', { params });
     // .toPromise().then(res => console.log(res));
     // .pipe(
     //   map(res => res['results']),
